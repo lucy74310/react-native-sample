faf042ec9724b06bcb16054a70ddbd1a59c89f96
lucy74310 <lucy74310@gmail.com>
2021-11-24 23:49:59 +0900

    sample

--- App.js
+++ App.js
@@ -6,8 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
+import React, {Fragment, Component} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -16,6 +15,7 @@ import {
   Text,
   useColorScheme,
   View,
+  Image,
 } from 'react-native';
 
 import {
@@ -26,86 +26,101 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+class IU extends Component {
+  render() {
+    let backImg = '';
+    if (this.props.type === 'one') {
+      backImg = require('./assets/back01.jpg');
+    } else if (this.props.type === 'two') {
+      backImg = require('./assets/wallpaperbetter.jpg');
+    }
+    return (
+      <View>
+        <Image source={backImg} style={{width: 100, height: 100}} />
+      </View>
+    );
+  }
+}
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <View style={styles.container}>
+          <IU type="two"></IU>
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+          <View style={{flexDirection: 'column'}}>
+            <View style={{flexDirection: 'row'}}>
+              <Text style={styles.title}>활동연대</Text>
+              <Text style={styles.detail}> 2000, 2010</Text>
+            </View>
+            <View style={{flexDirection: 'row'}}>
+              <Text style={[styles.title, styles.point]}>출생</Text>
+              <Text style={styles.detail}>1993</Text>
+            </View>
+            <View style={{flexDirection: 'row'}}>
+              <Text style={styles.title}>활동유형</Text>
+              <Text style={styles.detail}>여성 | 솔로</Text>
+            </View>
+            <View style={{flexDirection: 'row'}}>
+              <Text style={styles.title}>활동장르</Text>
+              <Text style={styles.detail}>가요 > 발라드, 댄스</Text>
+            </View>
+            <View style={{flexDirection: 'row'}}>
+              <Text style={styles.title}>데뷔</Text>
+              <Text style={styles.detail}>2008</Text>
+            </View>
+          </View>
+        </View>
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
+        <View style={{width: 50, height: 50, backgroundColor: 'blue'}}></View>
+        <View
+          style={{width: 100, height: 100, backgroundColor: 'skyblue'}}></View>
         <View
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+            width: 150,
+            height: 150,
+            backgroundColor: 'steelblue',
+          }}></View>
+      </View>
+    );
+  }
+}
+// const App = () => {
+//   const isDarkMode = useColorScheme() === 'dark';
+
+//   const backgroundStyle = {
+//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.hello}>Hello Worlddd I'm Lucy.</Text>
+//       <IU type="one"></IU>
+//       <IU type="two"></IU>
+//     </View>
+//   );
+// };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    marginTop: 50,
+    marginLeft: 20,
+    // flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    flexDirection: 'row',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  title: {
+    marginLeft: 10,
+    fontWeight: 'bold',
+    color: 'grey',
+    fontSize: 15,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  point: {
+    color: 'red',
   },
-  highlight: {
-    fontWeight: '700',
+  detail: {
+    marginLeft: 10,
   },
 });
 
